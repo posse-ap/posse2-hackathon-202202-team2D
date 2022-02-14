5dca364939ef8879a0d9a980967db7f1b4250bc4
TaikiKunimoto <taiki416avicii@gmail.com>
2022-02-15 06:27:48 +0900

    読み込みエラーがでるから使えない

--- js/top.js
+++ js/top.js
@@ -85,23 +85,23 @@ $(function(){
 });
 
 
-$('.catch_text').addClass('fadeUp');
 // $('.catch_text').addClass('fadeUp');
-// $('.catch_texts').addClass('fadeUp');
+// // $('.catch_text').addClass('fadeUp');
+// // $('.catch_texts').addClass('fadeUp');
 
-// $(function(){
-    $(window).scroll(function (){
-        // console.log($(this).scrollTop());
+// // $(function(){
+//     $(window).scroll(function (){
+//         // console.log($(this).scrollTop());
 
-      $('.fadeUp').each(function(){
-        // var elementTop = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        // var windowHeight = $(window).height();
-        if (scroll > 400){
-          $(".fadeUp").addClass('scrollin');
-        }
-      });
-    });
+//       $('.fadeUp').each(function(){
+//         // var elementTop = $(this).offset().top;
+//         var scroll = $(window).scrollTop();
+//         // var windowHeight = $(window).height();
+//         if (scroll > 400){
+//           $(".fadeUp").addClass('scrollin');
+//         }
+//       });
+//     });
 //   });
 
 // $('.toggle_btn').click(function () {
@@ -145,3 +145,33 @@ $('.catch_text').addClass('fadeUp');
 //     });
 //   });
 // });
+
+
+// ここからクニモト
+// 文章パートのフェードイン
+$(function () {
+  const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+  const fade_move = 100; // どのぐらい要素を動かすか(px)
+  const fade_time = 800; // フェードの時間(ms)
+  // フェード前のcssを定義
+  $(".fadein").css({
+    opacity: 0,
+    transform: "translateY(" + fade_move + "px)",
+    transition: fade_time + "ms",
+  });
+  // スクロールまたはロードするたびに実行
+  $(window).on("scroll load", function () {
+    const scroll_top = $(this).scrollTop();
+    const scroll_bottom = scroll_top + $(this).height();
+    const fade_position = scroll_bottom - fade_bottom;
+    $(".fadein").each(function () {
+      const this_position = $(this).offset().top;
+      if (fade_position > this_position) {
+        $(this).css({
+          opacity: 1,
+          transform: "translateY(0)",
+        });
+      }
+    });
+  });
+});
