f4c72722f19f01ee512782b315c798f080d2eeca
TaikiKunimoto <taiki416avicii@gmail.com>
2022-02-15 20:28:39 +0900

    ともあきのパスのミスを直した

--- js/top.js
+++ js/top.js
@@ -2,7 +2,7 @@
 
 
 //logoの表示
-$(window).on('load',function(){
+$(window).on('load', function () {
   $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1秒（1000ms）待機してからフェードアウト
   $("#splash_logo").delay(900).fadeOut('slow');//ロゴを0.9秒（900ms）待機してからフェードアウト
 });
@@ -55,12 +55,12 @@ $(".toggle_btn").click(function () {
 });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
 
-$(function(){
+$(function () {
   /*=================================================
   ハンバーガーメニュー
   ===================================================*/
   // ハンバーガーメニューのクリックイベント
-  $('.toggle_btn').on('click', function() {
+  $('.toggle_btn').on('click', function () {
     // #headerにopenクラスが存在する場合
     if ($('#header').hasClass('open')) {
       // openクラスを削除
@@ -68,7 +68,7 @@ $(function(){
       // メニューが非表示になる
       $('#header').removeClass('open');
 
-    // #headerにopenクラスが存在しない場合
+      // #headerにopenクラスが存在しない場合
     } else {
       // openクラスを追加
       // openクラスを追加すると、openクラスのCSSが適応されるため、
@@ -78,7 +78,7 @@ $(function(){
   });
 
   // メニューが表示されている時に画面をクリックした場合
-  $('#mask').on('click', function() {
+  $('#mask').on('click', function () {
     // openクラスを削除して、メニューを閉じる
     $('#header').removeClass('open');
   });
@@ -107,31 +107,31 @@ $('.catch_text').addClass('fadeUp');
 //     });
 //   });
 
-function slideAnime(){
+function slideAnime() {
   //====下に動くアニメーションここから===
-    $('.fadeUp').each(function(){
-            var elemPos = $(this).offset().top-70;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll >= elemPos - windowHeight){
-            // 下から上へ表示するクラスを付与
-            // テキスト要素を挟む親要素（下）とテキスト要素を元位置でアニメーションをおこなう
-            $(this).addClass("slideAnimeDownUp");
-            // 要素を下枠外に移動しCSS アニメーションで下から元の位置に移動
-            $(this).children(".fadeUp span").addClass("slideAnimeUpDown");
-            // 子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
-            }else{
-            // 下から上へ表示するクラスを取り除く
-            $(this).removeClass("slideAnimeDownUp");
-            $(this).children(".fadeUp span").removeClass("slideAnimeUpDown");
-            }
-        });
-  }
-  
-  // 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function (){
-    slideAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  $('.fadeUp').each(function () {
+    var elemPos = $(this).offset().top - 70;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      // 下から上へ表示するクラスを付与
+      // テキスト要素を挟む親要素（下）とテキスト要素を元位置でアニメーションをおこなう
+      $(this).addClass("slideAnimeDownUp");
+      // 要素を下枠外に移動しCSS アニメーションで下から元の位置に移動
+      $(this).children(".fadeUp span").addClass("slideAnimeUpDown");
+      // 子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+    } else {
+      // 下から上へ表示するクラスを取り除く
+      $(this).removeClass("slideAnimeDownUp");
+      $(this).children(".fadeUp span").removeClass("slideAnimeUpDown");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  slideAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 
 //   });
@@ -179,34 +179,84 @@ function slideAnime(){
 // });
 
 jQuery(function () {
-    var appear = false;
-    var pagetop = $('#page_top');
-    var goForm = $('.goForm');
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 400) {  //400pxスクロールしたら
-            if (appear == false) {
-                appear = true;
-                pagetop.stop().animate({
-                    'bottom': '20vw' //下から100pxの位置に
-                }, 400); //0.3秒かけて現れる
-                goForm.stop().animate({
-                    'bottom': '1vw' //下から100pxの位置に
-                }, 400); //0.3秒かけて現れる
-            }
-        } else {
-            if (appear) {
-                appear = false;
-                pagetop.stop().animate({
-                    'bottom': '-10vw' //下から-100pxの位置に
-                }, 300); //0.3秒かけて隠れる
-                goForm.stop().animate({
-                    'bottom': '-25vw' //下から-100pxの位置に
-                }, 300); //0.3秒かけて隠れる
-            }
-        }
-    });
-    pagetop.click(function () {
-        $('body, html').animate({ scrollTop: 0 }, 300); //0.5秒かけてトップへ戻る
-        return false;
-    });
+  var appear = false;
+  var pagetop = $('#page_top');
+  var goForm = $('.goForm');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {  //400pxスクロールしたら
+      if (appear == false) {
+        appear = true;
+        pagetop.stop().animate({
+          'bottom': '20vw' //下から100pxの位置に
+        }, 400); //0.3秒かけて現れる
+        goForm.stop().animate({
+          'bottom': '1vw' //下から100pxの位置に
+        }, 400); //0.3秒かけて現れる
+      }
+    } else {
+      if (appear) {
+        appear = false;
+        pagetop.stop().animate({
+          'bottom': '-10vw' //下から-100pxの位置に
+        }, 300); //0.3秒かけて隠れる
+        goForm.stop().animate({
+          'bottom': '-25vw' //下から-100pxの位置に
+        }, 300); //0.3秒かけて隠れる
+      }
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 300); //0.5秒かけてトップへ戻る
+    return false;
+  });
 });
+
+// Quote Generator
+
+var picture =[
+  '../img/eventphoto1',
+  '../img/eventphoto2',
+  '../img/eventphoto3',
+  '../img/eventphoto4',
+  '../img/eventphoto5',
+  '../img/eventphoto6',
+]
+
+var eventName = [
+  'eventName1',
+  'eventName2',
+  'eventName3',
+  'eventName4',
+  'eventName5',
+  'eventName6',
+] 
+
+let quote = document.getElementById('quote');
+
+quote.insertAdjacentHTML("beforeend",
+  `<div class="wrapper">`
+  + `<header>POSSE②の日常</header>`
+  + `<div class="content">`
+  + `<div class="img_area">`
+  + `<div><img src="${picture[r]}" alt=""></div>`
+  + `</div>`
+  + `</div>`
+  + `<div class="event">`
+  + `<span>__</span>`
+  + `<span class="name">${eventName[r]}</span>`
+  + `</div>`
+  + `</div>`
+  + `<div class="buttons">`
+  + `<div class="features">`
+  + `<div>`
+  + `<a href="">POSSE②の歴史って？</a>`
+  + `</div>`
+  + `<button>New Photo</button>`
+  + `</div>`
+  + `</div>`
+);
+
+const button = document.querySelector("button");
+
+button.addEventListener("click",function(){
+})
